4fdf9832037032c1ee535d1323673f6180cbfeec
frosDfurret <64030476+frosDfurret@users.noreply.github.com>
2021-08-31 23:25:42 -0700

    nvm, this is prob last update (#6)

--- js/overlay.js
+++ js/overlay.js
@@ -2,7 +2,7 @@ const cover = document.getElementById("cover");
 const overlay = document.getElementById("overlay");
 const furret = document.getElementById("furret");
 var fCry = new Audio(
-  "https://cdn.glitch.com/e1c3d8b5-22bd-497f-8ad1-f179ddfa190d%2Ffurret.mp3?v=1627883383086"
+  "/assets/aud/furret.mp3"
 );
 var sinNum = 0;
 var sinAcc = 0.01;
